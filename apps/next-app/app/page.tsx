44f004a1883ac4a36cb2b5e90dec42f476363b4d
Ade Mahendra <hendraadem@gmail.com>
2025-02-03 19:40:53 +0700

    feat(fix): fix add marker function

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -6,10 +6,10 @@ import Maps from './components/maps/Maps-v2'
 export default function Home() {
     return (
         <main className="w-full flex flex-col justify-between h-screen relative bg-white">
-            <div className="w-full h-[90%] bottom-0">
-                <Maps mapHeight="80vh" />
+            <div className="w-full h-100vh bottom-0">
+                <Maps mapHeight="100vh" />
             </div>
-            <div className="absolute w-full h-auto shadow-[0px_-6px_39px_-30px_#cbd5e0] rounded-t-[15px] bg-white bottom-0">
+            {/* <div className="absolute w-full h-auto shadow-[0px_-6px_39px_-30px_#cbd5e0] rounded-t-[15px] bg-white bottom-0">
                 <div className="sheet-body p-4">
                     <div className="sheet-header border-gray-100">
                         <h2 className="text-center font-medium text-[22px] text-gray-700">
@@ -18,7 +18,7 @@ export default function Home() {
                     </div>
                     <BottomMenu />
                 </div>
-            </div>
+            </div> */}
         </main>
     )
 }
