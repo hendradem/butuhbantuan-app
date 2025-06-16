f756c74807874e87c4c73ef908c5fde8fb5d6351
Ade Mahendra <hendraadem@gmail.com>
2025-06-16 12:55:40 +0700

    feat(fix): emergency store types

--- apps/next-app/app/store/useEmergencyData.ts
+++ apps/next-app/app/store/useEmergencyData.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import { EmergencyDataType } from '../types/emergency'
 
 type SelectedEmergencyDataType = {
-    selectedEmergencyData: EmergencyDataType
+    selectedEmergencyData: any
     selectedEmergencyType?: any
     selectedEmergencySource: 'map' | 'detail'
 }
