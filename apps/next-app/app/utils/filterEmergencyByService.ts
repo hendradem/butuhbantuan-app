ee937735c5e5d9bd1c5979aed35ef500074939d2
Ade Mahendra <hendraadem@gmail.com>
2025-07-23 08:21:02 +0700

    feat(fix): filterEmergencyService item types

--- apps/next-app/app/utils/filterEmergencyByService.ts
+++ apps/next-app/app/utils/filterEmergencyByService.ts
@@ -3,7 +3,7 @@ import useEmergency from '@/store/useEmergency'
 export const filterEmergencyByService = (serviceName: string) => {
     const emergency = useEmergency.getState().filteredEmergency
     const filtered = emergency.filter(
-        (item) => item.emergencyData.emergency_type.name === serviceName
+        (item: any) => item.emergencyData.emergency_type.name === serviceName
     )
 
     if (filtered.length > 0) {
