1cb3d8a0003c9fe3defc6681f86372c051a70356
Ade Mahendra <hendraadem@gmail.com>
2026-02-11 22:32:45 +0700

    remove node_modules from tracking

---
+++ apps/main/.nuxt/eslint.config.d.mts
@@ -0,0 +1,9 @@
+import type { FlatConfigComposer } from "../../../node_modules/.pnpm/eslint-flat-config-utils@3.0.1/node_modules/eslint-flat-config-utils/dist/index.mjs"
+import { defineFlatConfigs } from "../../../node_modules/.pnpm/@nuxt+eslint-config@1.15.1_@typescript-eslint+utils@8.55.0_eslint@10.0.0_jiti@2.6.1__typescri_m5m6bisipnyv2fuvvsrryjsmkm/node_modules/@nuxt/eslint-config/dist/flat.mjs"
+import type { NuxtESLintConfigOptionsResolved } from "../../../node_modules/.pnpm/@nuxt+eslint-config@1.15.1_@typescript-eslint+utils@8.55.0_eslint@10.0.0_jiti@2.6.1__typescri_m5m6bisipnyv2fuvvsrryjsmkm/node_modules/@nuxt/eslint-config/dist/flat.mjs"
+
+declare const configs: FlatConfigComposer
+declare const options: NuxtESLintConfigOptionsResolved
+declare const withNuxt: typeof defineFlatConfigs
+export default withNuxt
+export { withNuxt, defineFlatConfigs, configs, options }
---
+++ apps/main/.nuxt/nuxt.node.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="@nuxt/eslint" />
+/// <reference types="@nuxt/ui" />
+/// <reference types="@nuxt/devtools" />
+/// <reference types="@nuxt/telemetry" />
+/// <reference path="types/nitro-layouts.d.ts" />
+/// <reference path="types/modules.d.ts" />
+/// <reference path="types/runtime-config.d.ts" />
+/// <reference path="types/app.config.d.ts" />
+/// <reference types="nuxt" />
+/// <reference path="../../../node_modules/.pnpm/@nuxt+vite-builder@4.3.1_eslint@10.0.0_jiti@2.6.1__lightningcss@1.31.1_magicast@0.5.2_nuxt@4._6tqmtiy5dznjilvpxxfrj5hwa4/node_modules/@nuxt/vite-builder/dist/index.d.mts" />
+/// <reference path="../../../node_modules/.pnpm/@nuxt+nitro-server@4.3.1_db0@0.3.4_ioredis@5.9.2_magicast@0.5.2_nuxt@4.3.1_@parcel+watcher@2._ej6l3aa7wey7htf2xahzfj7u7m/node_modules/@nuxt/nitro-server/dist/index.d.mts" />
+/// <reference path="eslint-typegen.d.ts" />
+/// <reference path="types/nitro-middleware.d.ts" />
+/// <reference path="schema/nuxt.schema.d.ts" />
+
+export {}
