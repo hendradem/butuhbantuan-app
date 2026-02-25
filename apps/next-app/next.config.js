f1841f62fe6840f9c839587c4519ec9f1d0cae48
Ade Mahendra <hendraadem@gmail.com>
2026-02-26 00:07:18 +0700

    feat(fix): next cloudflare config

--- apps/next-app/next.config.js
+++ apps/next-app/next.config.js
@@ -7,7 +7,7 @@ const {
 module.exports = async (phase) => {
   /** @type {import("next").NextConfig} */
   const nextConfig = {
-  output: "export",
+  // output: "export",
   images: {
     domains: ["res.cloudinary.com", "bprugm.co.id"],
   },
