5d2a138e8eac5d5e60d0d04e6dcf19f5187749fc
Ade Mahendra <hendraadem@gmail.com>
2026-02-26 06:38:56 +0700

    fix: next-app config

--- apps/next-app/next.config.mjs
+++ apps/next-app/next.config.mjs
@@ -1,27 +1,16 @@
-import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from "next/constants.js";
+import withSerwist from "@serwist/next";
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: "edge",
-  },
   images: {
     domains: ["res.cloudinary.com", "bprugm.co.id"],
   },
   reactStrictMode: true,
 };
 
-export default async (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const { default: withSerwist } = await import("@serwist/next");
-
-    return withSerwist({
-      ...nextConfig,
-      swSrc: "app/service-worker/app-worker.ts",
-      swDest: "public/sw.js",
-      reloadOnOnline: true,
-    });
-  }
-
-  return nextConfig;
-};
+export default withSerwist({
+  swSrc: "app/service-worker/app-worker.ts",
+  swDest: "public/sw.js",
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
