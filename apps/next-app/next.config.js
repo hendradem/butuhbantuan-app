f14eaace2443e0a3e509e822f37c7343b91d6ca3
Ade Mahendra <hendraadem@gmail.com>
2026-02-26 03:04:52 +0700

    feat(deploy): cloudflare deploymeny config

--- apps/next-app/next.config.js
+++ apps/next-app/next.config.js
@@ -8,20 +8,29 @@ module.exports = async (phase) => {
   /** @type {import("next").NextConfig} */
   const nextConfig = {
   // output: "export",
+  experimental: {
+    runtime: 'edge',
+  },
   images: {
     domains: ["res.cloudinary.com", "bprugm.co.id"],
   },
   reactStrictMode: true,
 };
 
-if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-  const withSerwist = (await import("@serwist/next")).default({
-    swSrc: "app/service-worker/app-worker.ts",
-    swDest: "public/sw.js",
-    reloadOnOnline: true,
-  });
-  return withSerwist(nextConfig);
-}
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      swSrc: "app/service-worker/app-worker.ts",
+      swDest: "public/sw.js",
+      reloadOnOnline: true,
+    });
+    return withSerwist(nextConfig);
+  }
 
+  
   return nextConfig;
 };
+
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
