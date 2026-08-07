f090e9d4862b4ff7944a32d567e3c9572ec32300
Ade Mahendra <hendraadem@gmail.com>
2026-08-07 23:29:39 +0700

    init

--- apps/next-app/app/utils/setViewportHeight.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-export function setRealViewportHeight() {
-    if (window) {
-        const vh = window.innerHeight * 0.01
-        document.documentElement.style.setProperty('--vh', `${vh}px`)
-    }
-}
--- services/backend/internal/handler/user.handler.go
+++
@@ -1,7 +0,0 @@
-package handler
-
-import "github.com/gofiber/fiber/v2"
-
-func GetAllUsers(c *fiber.Ctx) error {
-	return c.SendString("User List!")
-}
